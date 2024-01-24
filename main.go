83345ce777c27a35729f9b92a629555e451545fd
ethan <enel@uscs.edu>
2024-01-24 09:19:03 -0800

    Added initial hello world for gin framework

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
+import "github.com/gin-gonic/gin"
+import "net/http"
+
 import (
 	"fmt"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+  fmt.Println("Running at http://localhost:8080")
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
