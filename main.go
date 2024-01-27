a2010823365b62c38f66f2b50a4f433cbb39d8f3
aasch2020 <a.asch2020@gmail.com>
2024-01-26 18:21:11 -0800

    sql create done

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import "github.com/gin-gonic/gin"
 import "net/http"
+import "os"
+import "github.com/jmoiron/sqlx"
+import _"github.com/mattn/go-sqlite3"
 
 import (
 	"fmt"
@@ -14,6 +17,27 @@ func main() {
 			"message": "pong",
 		})
 	})
+    // exactly the same as the built-in
+	schemacreate, erro := os.ReadFile("schema.sql")
+	if(erro!=nil){
+		fmt.Println("breaky")
+	}
+	fmt.Println(string(schemacreate))
+    db,err := sqlx.Open("sqlite3", ":memory:")
+	if err != nil{
+		fmt.Println("breaky")
+	}
+     
+    // force a connection and test that it worked
+    swagmoney := db.Ping()
+
+	db.MustExec(string(schemacreate))
+	if swagmoney != nil{
+		fmt.Println("breaky")
+	}else{
+		fmt.Println("not breaky")
+	}
+     
   fmt.Println("Running at http://localhost:8080")
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
---
+++ schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE UserTable (
+    UserID VARCHAR(255) PRIMARY KEY -- Assuming Auth0 provides a string-based user ID
+);
+
+CREATE TABLE TaskTable (
+    TaskID INT PRIMARY KEY,
+    UserID VARCHAR(255),
+    Category VARCHAR(255),
+    TaskName VARCHAR(255) NOT NULL,
+    Description TEXT,
+    StartTime DATETIME check(StartTime >= 0 and StartTime <= 24),
+    EndTime DATETIME check(EndTime >= 0 and EndTime <= 24),
+    IsCompleted BOOLEAN,
+    IsRecurring BOOLEAN,
+    IsAllDay BOOLEAN,
+    FOREIGN KEY (UserID) REFERENCES UserTable(UserID)
+);
+
+CREATE TABLE RecurrencePatterns (
+    TaskID INT,
+    RecurringType VARCHAR(15) CHECK(RecurringType IN ('daily','weekly','monthly')),
+    DayOfWeek INT check(DayOfWeek >= 0 and DayOfWeek <= 7),
+    DayOfMonth INT check(DayOfMonth >= 0 and DayOfMonth <= 31),
+    PRIMARY KEY (TaskID, RecurringType, DayOfWeek, DayOfMonth),
+    FOREIGN KEY (TaskID) REFERENCES TaskTable(TaskID)
+);
