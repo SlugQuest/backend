884e61bb90325d9e610b100495fbaf5206db0bec
sneha <55897319+sneha-afk@users.noreply.github.com>
2024-01-27 21:38:35 -0800

    calling ensurevalidtoken in main.go

--- main.go
+++ main.go
@@ -1,18 +1,31 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jmoiron/sqlx"
+	"slugquest.com/backend/middleware"
 
 	"fmt"
 
 	_ "github.com/mattn/go-sqlite3"
+
+	adapter "github.com/gwatts/gin-adapter"
+	envfuncs "github.com/joho/godotenv"
 )
 
 func main() {
+	// Load .env
+	if err := envfuncs.Load(); err != nil {
+		log.Fatalf("Error loading the .env file: %v", err)
+	}
+
+	// Wrapper for using standard http functions with gin
+	nextHandler, wrapper := adapter.New()
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
@@ -24,7 +37,8 @@ func main() {
 	if erro != nil {
 		fmt.Println("breaky")
 	}
-	fmt.Println(string(schemacreate))
+
+	// fmt.Println(string(schemacreate))
 	db, err := sqlx.Open("sqlite3", ":memory:")
 	if err != nil {
 		fmt.Println("breaky")
@@ -42,17 +56,20 @@ func main() {
 
 	//Router: takes incoming requests and routes them to functions to handle them
 	//Building a group of routes starting with this path
+
+	validateGinWrap := wrapper(middleware.EnsureValidToken()(nextHandler))
+
 	v1 := r.Group("/main/blah") //TODO: FIX the route and the uri's below
 	{
-		v1.GET("tasks", getAllUserTasks)
-		v1.GET("task/:id", getTaskById)
-		v1.POST("tasks", createTask)
-		v1.PUT("tasks/:id", editTask)
-		v1.DELETE("tasks/:id", deleteTask)
+		v1.GET("tasks", validateGinWrap, getAllUserTasks)
+		v1.GET("task/:id", validateGinWrap, getTaskById)
+		v1.POST("tasks", validateGinWrap, createTask)
+		v1.PUT("tasks/:id", validateGinWrap, editTask)
+		v1.DELETE("tasks/:id", validateGinWrap, deleteTask)
 
 	}
 
-	fmt.Println("Running at http://localhost:8080")
+	log.Print("Running at http://localhost:8080")
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
 
