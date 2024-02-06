a9b8ee599962cc501907aa48821a1a7c1ab13b8b
ananyabatra <ananyabatra04@gmail.com>
2024-02-05 21:02:10 -0800

    schema update

--- schema.sql
+++ schema.sql
@@ -1,36 +1,44 @@
 CREATE TABLE IF NOT EXISTS UserTable (
-    UserID VARCHAR(255) PRIMARY KEY,
-    Points INTEGER,
-    BossId INTEGER,
+    UserID VARCHAR(255) PRIMARY KEY NOT NULL,
+    Points INTEGER NOT NULL,
+    BossId INTEGER NOT NULL,
     FOREIGN KEY (BossId) REFERENCES BossTable(BossID)
 );
 
 CREATE TABLE TaskTable (
-    TaskID INTEGER PRIMARY KEY AUTOINCREMENT,
+    TaskID INT PRIMARY KEY AUTOINCREMENT,
     UserID VARCHAR(255) NOT NULL,
-    Category VARCHAR(255) NOT NULL,
+    Category INT NOT NULL,
     TaskName VARCHAR(255) NOT NULL,
     Description TEXT NOT NULL,
-    StartTime DATETIME,
-    EndTime DATETIME,
-    IsCompleted BOOLEAN NOT NULL,
+    StartTime DATETIME, -- optional
+    EndTime DATETIME, --optional 
+    Status VARCHAR(15) CHECK(Status IN ('completed','failed')),
     IsRecurring BOOLEAN NOT NULL,
     IsAllDay BOOLEAN NOT NULL,
-    FOREIGN KEY (UserID) REFERENCES UserTable(UserID)
+    Points INTEGER NOT NULL, -- Backend does conversion of easy/medium/hard to points
+    CronExpression VARCHAR(255) NOT NULL,
+    FOREIGN KEY (UserID) REFERENCES UserTable(UserID),
+    FOREIGN KEY (Category) REFERENCES Category(CatID)
 );
 
-CREATE TABLE RecurrencePatterns (
-    TaskID INT NOT NULL,
-    RecurringType VARCHAR(15) CHECK(RecurringType IN ('daily','weekly','monthly')),
-    DayOfWeek INT check(DayOfWeek >= 0 and DayOfWeek <= 7),
-    DayOfMonth INT check(DayOfMonth >= 0 and DayOfMonth <= 31),
-    PRIMARY KEY (TaskID, RecurringType, DayOfWeek, DayOfMonth),
-    FOREIGN KEY (TaskID) REFERENCES TaskTable(TaskID)
-);
 
 CREATE TABLE BossTable (
     BossID INTEGER PRIMARY KEY AUTOINCREMENT,
     BossName VARCHAR(255) NOT NULL,
-    HEALTH INTEGER
-    BossImage VARCHAR(255) --FileName
+    HEALTH INTEGER NOT NULL,
+    BossImage VARCHAR(255) NOT NULL--FileName
 );
+
+CREATE TABLE Category (
+	CatID INTEGER PRIMARY KEY AUTOINCREMENT,
+	Color INT NOT NULL -- hexcode? Ask frontend	
+);
+
+CREATE TABLE TrophyTable (
+    TrophyID INTEGER PRIMARY KEY AUTOINCREMENT,
+    UserID VARCHAR(255),
+    TrophyName VARCHAR (255),
+    FOREIGN KEY (UserID) REFERENCES UserTable(UserID)
+);
+
